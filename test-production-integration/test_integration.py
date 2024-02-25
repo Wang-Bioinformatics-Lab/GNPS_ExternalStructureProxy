583d04965b9330cc1d8b7d649fa0aee72377d288
Ming Wang <mwang87@gmail.com>
2024-02-24 17:22:08 -0800

    Update test_integration.py

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -88,7 +88,3 @@ def test_redirects():
     r.raise_for_status()
 
 
-def test_ftp_proxy():
-    url = f"https://{PRODUCTION_URL}/massiveftpproxy?ftppath=ftp://massive.ucsd.edu/MSV000085699/peak/S6.mgf"
-    r = requests.get(url)
-    r.raise_for_status()
