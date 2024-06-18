541cb9fc397d7177a5c08b858234087deaa5b936
Ming Wang <mwang87@gmail.com>
2024-06-18 14:53:09 -0700

    Update test_integration.py

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -8,36 +8,7 @@ def test_production():
     r = requests.get(url)
     r.raise_for_status()
 
-
-
-def test_gnps_library():
-    url = f"https://{PRODUCTION_URL}/gnpslibraryjson"
-    r = requests.get(url)
-    r.raise_for_status()
-
-    url = f"https://{PRODUCTION_URL}/gnpslibraryformattedjson"
-    r = requests.get(url)
-    spectra_list = r.json()
-    assert(len(spectra_list) > 10)
-    r.raise_for_status()
-
-    # Likely Too Big
-#     url = f"https://{PRODUCTION_URL}/gnpslibraryformattedwithpeaksjson"
-#     r = requests.get(url)
-#     spectra_list = r.json()
-#     assert(len(spectra_list) > 10)
-#     r.raise_for_status()
-
-    url = f"https://{PRODUCTION_URL}/gnpslibraryfornpatlasjson"
-    r = requests.get(url)
-    spectra_list = r.json()
-    assert(len(spectra_list) > 10)
-    r.raise_for_status()
-
-    url = f"https://{PRODUCTION_URL}/gnpslibraryfornpatlastsv"
-    r = requests.get(url)
-    r.raise_for_status()
-    
+def test_gnps_library():    
     url = f"https://{PRODUCTION_URL}/gnpslibrary/GNPS-LIBRARY.mgf"
     r = requests.get(url)
     r.raise_for_status()
