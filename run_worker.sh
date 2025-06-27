d605187fcfae11ce2c6fb9fb5875cb0e7e5d631b
Mingxun Wang <mwang87@gmail.com>
2025-06-27 14:35:51 -0700

    fix log level

--- run_worker.sh
+++ run_worker.sh
@@ -4,5 +4,5 @@
 source activate rdkit && python create_tables.py
 
 # Running the actual worker
-source activate rdkit && celery -A tasks_worker worker -c 1 -Q worker --loglevel DEBUG --max-tasks-per-child 1000
+source activate rdkit && celery -A tasks_worker worker -c 1 -Q worker --loglevel INFO --max-tasks-per-child 1000
 
