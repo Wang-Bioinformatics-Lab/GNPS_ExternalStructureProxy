6f83c9ed20584fa90f331f83725ecaac2ff9137a
Mingxun Wang <mwang87@gmail.com>
2025-06-27 18:57:52 -0700

    setting timeouts

--- run_production_server.sh
+++ run_production_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source activate web && gunicorn -w 24 --threads=4 -b 0.0.0.0:5000 --timeout 600 main:app
+source activate web && gunicorn -w 24 --threads=4 -b 0.0.0.0:5000 --timeout 60 main:app
