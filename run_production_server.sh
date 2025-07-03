8a24e7411a3541452ef1f476db8c64ace0897924
Mingxun Wang <mwang87@gmail.com>
2025-07-03 16:22:06 -0700

    spacing

--- run_production_server.sh
+++ run_production_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 source activate web && gunicorn -w 24 --threads=4 -b 0.0.0.0:5000 --timeout 60 main:app --max-requests 10000 --max-requests-jitter 1000 --access-logfile /app/logs/access.log
